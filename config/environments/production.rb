e13a7f94f5d944351266ada1dfb9b37375f6b5e3
Mariam <mariam.antar@hotmail.com>
2018-07-24 14:44:20 +1000

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
