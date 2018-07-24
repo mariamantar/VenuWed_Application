13626ef306148844a60956cd4bfc2a0b3892bb6b
Mariam <mariam.antar@hotmail.com>
2018-07-24 14:39:26 +1000

    heroku push asset pipeline upadte

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ module Venuwed
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-config.assets.initialize_on_precompile = false
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
